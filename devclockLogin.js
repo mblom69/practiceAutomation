48c6022dbe06d6ff7155dc4d25525eab0f89335a
mtblom <mtblom69@gmail.com>
2019-05-08 13:47:18 -0600

    add

---
+++ devclockLogin.js
@@ -0,0 +1,17 @@
+module.exports = {
+    beforeEach: browser => {
+        browser.url('https://cards-beta.devclock.com/login')
+        .waitForElementPresent('.content',5000)
+        },
+    after: browser => {
+        browser.end()
+    },
+    'Can log in with an existing account': browser =>{
+        browser
+        .setValue('input[type="email"]', 'test@devmtnqa.com')
+        .setValue('input[type="password"]', 'testpass')
+        .useXpath()
+        .click('//button[contains(@data-bind, "login")]')
+        .expect.element('//div[contains(@data-bind, "logout")]').to.be.visible.before(5000)
+    }
+}
